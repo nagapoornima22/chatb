7da73e24daa3a8fe3d06218523317d75f186cb6c
nagapoornima22 <naga.poornima22@gmail.com>
2020-03-03 13:34:04 +0530

    project files

---
+++ train.py
@@ -0,0 +1,15 @@
+from chatbot import chatbot
+from chatterbot.trainers import ListTrainer
+
+
+trainer = ListTrainer(chatbot)
+
+trainer.train([
+    "Hi there!",
+    "Hello",
+])
+
+trainer.train([
+    "Greetings!",
+    "Hello",
+])
